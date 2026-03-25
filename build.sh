3b52515442a06b3fbe1a0272f685898d451e9a54
sc-bin <3335447573@qq.com>
2026-03-25 18:08:59 +0800

    增加编译python支持

--- build.sh
+++ build.sh
@@ -48,6 +48,8 @@ find_python_version() {
 
 # 获取各个组件的路径
 PYTHON_VERSION=$(find_python_version "$PATH_ROOTFS")
+PYTHON3_VERSION_MAJOR=$(echo "$PYTHON_VERSION" | cut -d. -f1)
+PYTHON3_VERSION_MINOR=$(echo "$PYTHON_VERSION" | cut -d. -f2)
 NUMPY_INCLUDE_PATH="${PATH_ROOTFS}/usr/lib/python3/dist-packages/numpy/_core/include"
 NUMPY_VERSION=$(grep -oE '([0-9]+\.[0-9]+)' "${NUMPY_INCLUDE_PATH}/numpy/version.py" 2>/dev/null || echo "1.21")
 
@@ -56,9 +58,22 @@ NUMPY_VERSION=$(grep -oE '([0-9]+\.[0-9]+)' "${NUMPY_INCLUDE_PATH}/numpy/version
 # ============================================================================
 cmake ${OPENCV_SRC} \
     -DCMAKE_TOOLCHAIN_FILE=../toolchain-riscv64-k230.cmake \
-    -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
+    -DCMAKE_INSTALL_PREFIX=/usr \
     -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_SHARED_LIBS=ON 
+    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_opencv_python3=ON \
+    -DPYTHON3_VERSION_MAJOR=${PYTHON3_VERSION_MAJOR} \
+    -DPYTHON3_VERSION_MINOR=${PYTHON3_VERSION_MINOR} \
+    -DPYTHON3_EXECUTABLE=/usr/bin/python3 \
+    -DPYTHON3_INCLUDE_PATH="${PATH_ROOTFS}/usr/include/python${PYTHON_VERSION};${PATH_ROOTFS}/usr/include/riscv64-linux-gnu/;${PATH_ROOTFS}/usr/include/" \
+    -DPYTHON3_INCLUDE_DIR=${PATH_ROOTFS}/usr/include/python${PYTHON_VERSION} \
+    -DPYTHON3_LIBRARIES=${PATH_ROOTFS}/usr/lib/riscv64-linux-gnu/libpython${PYTHON_VERSION}.so \
+    -DPYTHON3_LIBRARY=${PATH_ROOTFS}/usr/lib/riscv64-linux-gnu/libpython${PYTHON_VERSION}.so \
+    -DPYTHON3_NUMPY_INCLUDE_DIRS=${PATH_ROOTFS}/usr/lib/python3/dist-packages/numpy/_core/include \
+    -DPYTHON3_PACKAGES_PATH=/usr/local/lib/python${PYTHON_VERSION}/dist-packages \
+    -DPYTHON3_NUMPY_VERSION=${NUMPY_VERSION} \
+    -DCMAKE_FIND_ROOT_PATH=${PATH_ROOTFS} \
+    -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY
 
 # 检查 CMake 配置是否成功
 if [ $? -ne 0 ]; then
@@ -69,7 +84,15 @@ fi
 # 编译和安装
 # ============================================================================
 make -j$(nproc) VERBOSE=1
-make install
+
+rm -rf ${INSTALL_DIR}
+make install DESTDIR=${INSTALL_DIR}
+
+DIST_SO_DIR=${INSTALL_DIR}/usr/local/lib/python${PYTHON_VERSION}/dist-packages/cv2/python-${PYTHON_VERSION}/
+# 将DIST_SO_DIR路径下的文件重命名为cv2.cpython-313-riscv64-linux-gnu.so
+for file in ${DIST_SO_DIR}*.so; do
+    mv "$file" "${DIST_SO_DIR}cv2.cpython-${PYTHON3_VERSION_MAJOR}${PYTHON3_VERSION_MINOR}-riscv64-linux-gnu.so"
+done
 
 echo "========================================"
 echo "编译完成！安装路径: ${INSTALL_DIR}"
